9754a4826d27aa7d78eec16996e587d656aa34f3
AshishYadav43 <ashishyadav.eminence@gmail.com>
2025-08-05 17:47:11 +0530

    changes in sport

--- src/app/layout/sidebar.component.ts
+++ src/app/layout/sidebar.component.ts
@@ -6,16 +6,23 @@ import { Router, RouterModule } from '@angular/router';
 
 import { AuthService } from '../core/services/auth.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, MatIconModule, RouterModule],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, RouterModule, NgIf,NgFor],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  showAddDefaults = false;
 
+  addDefaultsChildren = [
+    { name: 'Country', route: '/countries' },
+    { name: 'Currency', route: '/currencies' },
+    { name: 'Company', route: '/company' }
+  ];
 
 
   private router = inject(Router);
@@ -26,5 +33,9 @@ export class SidebarComponent {
         this.router.navigate(['/login']);
       }
     })
+  }
+
+    toggleAddDefaults() {
+    this.showAddDefaults = !this.showAddDefaults;
   }
 }
